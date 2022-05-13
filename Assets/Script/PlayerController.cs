3e48b295edb7721ad1a1a0344a13f67ff0c3b039
RizkyHendraSaputra <101460749+4ENDSTUDIO@users.noreply.github.com>
2022-05-14 02:59:25 +0700

    p

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    faceDetector FaceDetector;
+    float speed = 5f;
+    float lastY = 0;
+    void Start()
+    {
+        FaceDetector = (faceDetector)FindObjectOfType(typeof(faceDetector));
+    }
+
+    // Update is called once per frame
+    public void Update()
+
+    {
+
+        float step = speed * Time.deltaTime;
+
+
+
+
+        float norm = Mathf.Clamp(FaceDetector.faceY - lastY, -1, 1);
+
+        //below transform code is in one line
+
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y + norm, transform.position.z), step);
+
+
+        //below line was additional added, then the character starts moving
+        lastY = FaceDetector.faceY;
+
+
+
+
+
+    }
+}
+    
